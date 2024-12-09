a7dc0de33eea757da057c32dc3c85652ee26ace9
Yasasri077 <yasasrichaparala4@gmail.com>
2024-12-09 15:05:01 +0530

    Add files via upload

---
+++ Spring_Rest-API/src/test/java/com/JFSD/Spring_Rest_API/SpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JFSD.Spring_Rest_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
